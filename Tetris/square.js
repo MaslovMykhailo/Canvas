244de74f38bb49b3bf4c03ae0ef1491e606399ce
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-01-07 14:56:36 +0200

    Create class Square and LastSquares array

---
+++ Tetris/square.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class Square extends Rectangle {
+  constructor(x, y, color) {
+    super(x, y, 25, 25);
+    
+    this.color = color;
+  }
+  
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this._x, this._y, this._w, this._h);
+    ctx.closePath();
+  }
+}
+
+const lastSquares = [];
+
+for(let i = 0 ; i < 250 ; i += 25) {
+  lastSquares.push(new Square(i, 500, 'black'));
+}
