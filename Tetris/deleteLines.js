4b1347d2b7da05d53b741eec2dc38e708ad1beb6
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-01-09 20:07:04 +0200

    Create delete lines function

---
+++ Tetris/deleteLines.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const shiftLines = (arrOfLines) => {
+  let indexOfFullLines = [];
+  
+  arrOfLines.forEach(l => {
+    if(l.isFull) indexOfFullLines.push(l.i);
+  });
+  
+  arrOfLines.forEach(l => {
+    indexOfFullLines.forEach(i => {
+      if(l.i < i) {
+        l.arrOfSq.forEach(sq => {
+          sq.sety = sq.y + 25;
+        });
+      }
+    });
+  });
+  
+  return arrOfLines;
+};
+
+const findFullLines = (statSqs) => {
+  const arrOfLines = [];
+  for(let i = 1 ; i <= 20 ; i++) {
+    let y = 10 + 25 * (i - 1);
+    let segment = new Segment(0, y, 250, y);
+    
+    let arrOfSq = [];
+    statSqs.forEach(sq => {
+      if(intersect(segment, sq)) arrOfSq.push(sq);
+    });
+    
+    let isFull = false;
+    if(arrOfSq.length >= 10) {
+      isFull = true;
+      arrOfSq = [];
+    }
+    
+    arrOfLines.push({i, isFull, arrOfSq});
+  }
+  return arrOfLines;
+};
+
+const create = (alwaysStat, arrOfLines) => {
+  let res = alwaysStat.slice();
+  
+  arrOfLines.forEach(l => {
+    res = res.concat(l.arrOfSq);
+  });
+  
+  return res;
+};
+
+const newStatSquares = (statSqs, alwaysStat) => {
+  return create(alwaysStat, shiftLines(findFullLines(statSqs)));
+};
